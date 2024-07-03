1ec96ac2f33648790a341dc784187757cd3d0d2f
Divine Chibueze <divindavid6677@gmail.com>
2024-07-03 09:53:00 +0100

    Version History

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const axios = require("axios")
+require("dotenv").config()
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.set("trust proxy", true);
+
+app.get("/", (req,res) => {
+    res.send("Welcome to my server")
+})
+
+app.get("/api/hello", async (req, res) => {
+    const visitorName = req.query.visitor_name || "Guest";
+    const clientAddr = req.ip;
+
+    try {
+        const response = await axios.get( "http://api.weatherapi.com/v1/current.json", {
+        params: {
+        key: process.env.key,
+        q: clientAddr,
+        aqi: "no"
+        }});
+        
+        const clientCity = response.data.location.name 
+        const clientTemp = response.data.current.temp_c
+        
+        res.status(200).json({
+              client_ip: ipAddr,
+              location: clientCity,
+              greeting: `Hello, ${visitorName}!, the temperature is ${clientTemp} degrees Celcius in ${clientCity}`,});
+        
+        } catch (err) {
+        res.status(500).json({
+              error: err,
+              msg: "Something went wrong!",
+        });
+        }
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
